9ba82fbd3d4b5690049623cd354f0931676e1eb0
MrMonsterX <monsterking122333@gmail.com>
2024-03-12 12:00:53 +0530

    Increment version to 1.1.0 (VersionCode: 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.madinaappstudio.deviceanalyzer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
